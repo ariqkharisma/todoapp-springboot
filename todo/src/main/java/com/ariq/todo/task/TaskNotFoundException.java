3c06edf119a30aa4cd2007d2a0e20e226a930414
ariqkharisma <67833394+ariqkharisma@users.noreply.github.com>
2023-05-07 14:56:57 +0700

    Update TaskNotFoundException.java

--- todo/src/main/java/com/ariq/todo/task/TaskNotFoundException.java
+++ todo/src/main/java/com/ariq/todo/task/TaskNotFoundException.java
@@ -3,7 +3,7 @@ package com.ariq.todo.task;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "student not found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Task not found")
 public class TaskNotFoundException extends RuntimeException {
 
     public TaskNotFoundException() {
